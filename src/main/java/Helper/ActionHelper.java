7eeaba18fbabed7a2531e8ef6a65affbd731c1db
arpan <arpankushwahapro@gmail.com>
2025-05-01 21:40:38 +0530

    commit

--- src/main/java/Helper/ActionHelper.java
+++ src/main/java/Helper/ActionHelper.java
@@ -1,27 +1,26 @@
 package Helper;
 
-import common.Initializer;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-
-import java.io.IOException;
 
 public class ActionHelper {
 
     private WebDriver driver;
     private Actions action;
 
-    public ActionHelper() throws IOException {
-        driver = Initializer.getDriver();
+    public ActionHelper(WebDriver driver){
+        this.driver = driver;
         action = new Actions(this.driver);
     }
 
-    public void rightClickOn(WebElement obj) {
+    public void rightClickOn(WebElement obj){
         action.contextClick(obj).build().perform();
     }
 
-    public void doubleClickOn(WebElement obj) {
+    public void doubleClickOn(WebElement obj){
         action.moveToElement(obj).doubleClick().perform();
     }
 
